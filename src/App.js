92c0d914267d283a7999f69736462286bdafc3ae
nevroje <joakim.berglind@gmail.com>
2019-09-10 17:51:09 +0200

    add parallax effect according to Freecodecamp howto

--- src/App.js
+++ src/App.js
@@ -34,9 +34,45 @@ class App extends Component {
       </>
     );
   }
-  // TODO: make parallax webflow effect
+  // parallax effect according to FreeCodeCamp
   componentDidMount() {
+    const navbar = document.querySelector("#navbar");
+    const header = document.querySelector("#welcome-section");
+    const forest = document.querySelector(".forest");
+    let forestInitPos = -300;
 
+    window.onscroll = () => {
+      let scrollPos =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (scrollPos <= window.innerHeight) {
+        forest.style.bottom = `${parseInt(scrollPos / 6)}px`;
+      }
+
+      if (scrollPos - 100 <= window.innerHeight)
+        header.style.visibility =
+          header.style.visibility === "hidden" && "visible";
+      else header.style.visibility = "hidden";
+
+      if (scrollPos + 100 >= window.innerHeight)
+        navbar.classList.add("bg-active");
+      else navbar.classList.remove("bg-active");
+    };
+
+    (function navSmoothScrolling() {
+      const internalLinks = document.querySelectorAll('a[href^="#"]');
+      for (let i in internalLinks) {
+        if (internalLinks.hasOwnProperty(i)) {
+          internalLinks[i].addEventListener("click", e => {
+            e.preventDefault();
+            document.querySelector(internalLinks[i].hash).scrollIntoView({
+              block: "start",
+              behavior: "smooth"
+            });
+          });
+        }
+      }
+    });
   }
 }
 
