1d44fb5fd8d4716a15a352633263128c62b395be
nevroje <joakim.berglind@gmail.com>
2019-09-27 15:30:32 +0200

    commented out header h1

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,10 +7,10 @@ const Header = props => {
       <div className="forest" />
       <div className="moon" />
       <div className="container">
-      <h1>
+      {/* <h1>
         <span className="line">I</span>
         <span className="line">Code</span>
-      </h1>
+      </h1> */}
       <div className="buttons">
         <a href="#projects">my portfolio</a>
         <a href="#contact" className="cta">
